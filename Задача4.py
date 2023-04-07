4c429676885dab02a1255ce25b8724d266e0b51e
Yelnur <yelyadu@gmail.com>
2023-04-07 12:18:37 +0600

    Задача 2 готова

---
+++ Задача4.py
@@ -0,0 +1,4 @@
+a = int(input("Введите общее количество журавликов - "))
+Petya_i_Serezha = int (a/6)
+Katya = int (2* Petya_i_Serezha)
+print (f"Петя и Сережа по {Petya_i_Serezha} журавликов, а Катя {Katya}")
